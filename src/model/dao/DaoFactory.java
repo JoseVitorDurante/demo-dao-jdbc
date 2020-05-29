5eb176e3fefb94754d8fb74bffb970bbc464807d
José Vitor Durante <josevitor_limma@hotmail.com>
2020-05-29 16:33:13 -0300

    DepartmentDao implementation and test

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,15 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJBDBC;
 
 public class DaoFactory {
 
 	public static SellerDao createSellerDao(){
 		return new SellerDaoJBDBC(DB.getConnection());
+	}
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 }
--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -7,8 +7,8 @@ import model.entities.Department;
 public interface DepartmentDao {
 
 	void insert(Department obj);
-	void upddate(Department obj);
-	void deleteBy(Department obj);
+	void update(Department obj);
+	void deleteById(Integer id);
 	Department findById(Integer id);
 	List<Department> findAll();
 }
